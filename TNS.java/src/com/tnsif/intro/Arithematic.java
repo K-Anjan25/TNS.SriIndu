16c8f61fe28e13287bd16c38d113035fb18b355b
MANISH <MANISH@192.168.0.107>
2025-12-24 09:04:25 +0530

    arithematic.java

--- TNS.java/src/com/tnsif/intro/Arithematic.java
+++ TNS.java/src/com/tnsif/intro/Arithematic.java
@@ -1,11 +1,12 @@
 package com.tnsif.intro;
 
 public class Arithematic {
-
+    
 	public static void main(String [] args) {
 		int a=20;
 		int b=10;
 		int c;
+		//pushed to github
 		c=a+b;
 		System.out.println("Sum: "+c);
 		c=a-b;
