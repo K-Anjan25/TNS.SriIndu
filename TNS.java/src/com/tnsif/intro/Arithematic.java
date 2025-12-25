37c2d6578430ac27de73fde18fe2cc27851efab3
MANISH <MANISH@192.168.0.107>
2025-12-25 20:46:40 +0530

    added Arithematic.java

--- TNS.java/src/com/tnsif/intro/Arithematic.java
+++ TNS.java/src/com/tnsif/intro/Arithematic.java
@@ -1,21 +1,11 @@
 package com.tnsif.intro;
 
 public class Arithematic {
-    
 	public static void main(String [] args) {
 		int a=20;
 		int b=10;
 		int c;
 		c=a+b;
-		System.out.println("Sum: "+c);
-		c=a-b;
-		System.out.println("Sub: "+c);
-		c=a*b;
-		System.out.println("Mul: "+c);
-		c=a/b;
-		System.out.println("Div: "+c);
-		c=a%b;
-		System.out.println("Mod: "+c);
+		System.out.println("Result: "+c);
 	}
-
 }
