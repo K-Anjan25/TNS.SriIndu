b27d4de6c48483cbf8d7f13f048094a3ca356c45
MANISH <MANISH@192.168.0.107>
2025-12-26 12:56:11 +0530

    added constructor.java

---
+++ TNS.java/src/com/tnsif/java/constr/ConstrDemo.java
@@ -0,0 +1,11 @@
+package com.tnsif.java.constr;
+
+public class ConstrDemo {
+
+	public static void main(String[] args) {
+		Constr c = new Constr();
+		Child c1 = new Child();
+		c1.Constr();
+	}
+
+}
