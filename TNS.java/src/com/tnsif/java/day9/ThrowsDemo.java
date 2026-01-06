fa891433aaf4276ad99f7f7fa4e7d2b142054530
Anjan <MANISH@192.168.0.106>
2026-01-06 22:38:52 +0530

    added ThrowsDemo.java

---
+++ TNS.java/src/com/tnsif/java/day9/ThrowsDemo.java
@@ -0,0 +1,25 @@
+package com.tnsif.java.day9;
+
+class sample{
+	public void show() throws Exception {
+		try {
+			int a = 10,b = 0;
+			int c = a/b;
+			System.out.println(c);
+			
+			}finally {
+				System.out.println("This is Finally block");
+			}
+	}
+	
+}
+
+
+public class ThrowsDemo {
+
+	public static void main(String[] args) throws Exception {
+			sample s = new sample();
+			s.show();
+	}
+
+}
