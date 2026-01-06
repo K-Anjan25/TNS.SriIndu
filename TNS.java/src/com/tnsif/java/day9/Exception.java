b3d603e0a9e7fd032e8976941fedc1197af6ab1b
K-Anjan25 <132475784+K-Anjan25@users.noreply.github.com>
2026-01-06 21:49:31 +0530

    Add Exception handling examples in Exception.java

---
+++ TNS.java/src/com/tnsif/java/day9/Exception.java
@@ -0,0 +1,25 @@
+public class Exception{
+	public static void main(String [] args){
+		try{
+			int a,b;
+			a = 10;
+			b = 0;
+			int c = a/b;
+		}catch(Exception e){
+					System.out.println("The division is not possible");
+				}
+		try{
+			int[] arr = {1,2,3,4};
+			System.out.println(arr[0]);
+			System.out.println(arr[4]);
+		}catch(Exception e){
+					System.out.println("You can't read the value of arr[4]");
+				}
+		try{
+			String s = null;
+			System.out.println(s.length());
+		}catch(Exception e){
+					System.out.println("The String value is null");
+				}
+	}
+}
