b240c1fd3b0fee4f5f76d7dac44cbe15c75c59d3
K-Anjan25 <132475784+K-Anjan25@users.noreply.github.com>
2026-01-06 22:08:47 +0530

    Create ThrowExceptionDemo to check voting eligibility

---
+++ TNS.java/src/com/tnsif/java/day9/ThrowExceptionDemo.java
@@ -0,0 +1,14 @@
+package com.tnsif.java.day9;
+
+public class ThrowExceptionDemo{
+	public static void main(String [] args){
+		int age = 12;
+		if(age > 18)
+		{
+			System.out.println("Eligible for voting");
+		}
+		else{
+			throw new ArithematicException("The age is < 18 so not Eligible for voting");
+		}
+	}
+}
