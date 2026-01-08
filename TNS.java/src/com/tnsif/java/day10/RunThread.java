19cfbadd57eaab5db39516f04f8dce26cfcccd16
Anjan <MANISH@192.168.0.106>
2026-01-08 16:17:20 +0530

    added RunThread.java

--- TNS.java/src/com/tnsif/java/day10/RunThread.java
+++ TNS.java/src/com/tnsif/java/day10/RunThread.java
@@ -2,7 +2,6 @@ package com.tnsif.java.day10;
 
 public class RunThread implements Runnable{
 
-	@override
 	public void run()
 	{
 		System.out.println("This is current thread implementing Runnable.");
