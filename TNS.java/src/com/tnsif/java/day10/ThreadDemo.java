0daa11516288232b659b2df4eb8e80b7010d869c
Anjan <MANISH@192.168.0.106>
2026-01-08 15:35:45 +0530

    added ThreadDemo.java

--- TNS.java/src/com/tnsif/java/day10/ThreadDemo.java
+++ TNS.java/src/com/tnsif/java/day10/ThreadDemo.java
@@ -1,10 +1,10 @@
 package com.tnsif.java.day10;
 
 public class ThreadDemo{
-	public static void main(String [] args)
+	public static void main(String [] args) throws InterruptedException
 	{
 		for(int i=0;i<5;i++){
-			MultiThreads mt = new MultiThreads()
+			MultiThreads mt = new MultiThreads();
 			mt.start();
 			mt.sleep(5000);
 		}
