f708a6801380a27dea38e0bb33f52138fb8fb753
K-Anjan25 <132475784+K-Anjan25@users.noreply.github.com>
2026-01-08 16:08:07 +0530

    Add RunThread class implementing Runnable interface

---
+++ TNS.java/src/com/tnsif/java/day10/RunThread.java
@@ -0,0 +1,11 @@
+package com.tnsif.java.day10;
+
+public class RunThread implements Runnable{
+
+	@override
+	public void run()
+	{
+		System.out.println("This is current thread implementing Runnable.");
+	}
+}
+
