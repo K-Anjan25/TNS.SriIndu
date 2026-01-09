4eeedcc25115846d1332b51bd6997fe21297c0c3
K-Anjan25 <132475784+K-Anjan25@users.noreply.github.com>
2026-01-09 15:57:22 +0530

    Add ThreadDemoClass for message delivery and reception

---
+++ TNS.java/src/com/tnsif/java/day11/ThreadDemoClass.java
@@ -0,0 +1,39 @@
+package com.tnsif.java.day11;
+
+public class ThreadDemoClass{
+	
+	int i;
+	boolean flag=false;
+
+	synchronized void deliver(int i)
+	{
+		if(flag){
+			try{
+				wait();
+			}catch(Exception e){
+				System.err.println(e);
+			}
+		}
+		this.i=1;
+		flag = true;
+		System.out.println("The msg is delivered "+i);
+		notify();
+	}
+
+	synchronized void receiver(int i)
+	{
+		if(!flag){
+			try{
+				wait();
+			}catch(Exception e){
+				System.err.println(e);
+			}
+		}
+		System.out.println("The msg is received "+i);
+		flag = false;
+		notify();
+		return i;
+	}
+}
+		
+		
