6edc8cf136611eca1d869fa009ddc1bf370a97fb
K-Anjan25 <132475784+K-Anjan25@users.noreply.github.com>
2026-01-25 13:55:22 +0530

    Create Calculator.java

---
+++ TNS.java/src/com/tnsif/applications/Calculator.java
@@ -0,0 +1,36 @@
+package com.tnsif.applications;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first number:");
+        double num1 = scanner.nextDouble();
+        System.out.println("Enter operator (+, -, *, /):");
+        char operator = scanner.next().charAt(0);
+        System.out.println("Enter second number:");
+        double num2 = scanner.nextDouble();
+
+        switch (operator) {
+            case '+':
+                System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+                break;
+            case '-':
+                System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+                break;
+            case '*':
+                System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
+                break;
+            case '/':
+                if (num2 != 0) {
+                    System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
+                } else {
+                    System.out.println("Cannot divide by zero!");
+                }
+                break;
+            default:
+                System.out.println("Invalid operator!");
+        }
+    }
+}
