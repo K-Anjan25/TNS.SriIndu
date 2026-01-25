8bef72cc51f89b113b45fba084e76b3bcfb76421
K-Anjan25 <132475784+K-Anjan25@users.noreply.github.com>
2026-01-25 23:10:29 +0530

    Add Sudoku solver implementation

---
+++ TNS.java/src/com/tnsif/applications/Sudoku.java
@@ -0,0 +1,94 @@
+package com.tnsif.applications;
+
+public class Sudoku {
+    public static void solve(int[][] board) {
+        solve(board, 0, 0);
+    }
+
+    private static boolean solve(int[][] board, int row, int col) {
+        if (col == 9) {
+            col = 0;
+            row++;
+            if (row == 9) {
+                return true;
+            }
+        }
+
+        if (board[row][col] != 0) {
+            return solve(board, row, col + 1);
+        }
+
+        for (int num = 1; num <= 9; num++) {
+            if (isValid(board, row, col, num)) {
+                board[row][col] = num;
+                if (solve(board, row, col + 1)) {
+                    return true;
+                }
+                board[row][col] = 0;
+            }
+        }
+
+        return false;
+    }
+
+    private static boolean isValid(int[][] board, int row, int col, int num) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == num || board[i][col] == num) {
+                return false;
+            }
+        }
+
+        int startRow = row - row % 3;
+        int startCol = col - col % 3;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i + startRow][j + startCol] == num) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public static void printBoard(int[][] board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(board[i][j] + " ");
+                if ((j + 1) % 3 == 0 && j < 8) {
+                    System.out.print("| ");
+                }
+            }
+            System.out.println();
+            if ((i + 1) % 3 == 0 && i < 8) {
+                System.out.println("--------+---------+--------");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] board = {
+                {5, 3, 0, 0, 7, 0, 0, 0, 0},
+                {6, 0, 0, 1, 9, 5, 0, 0, 0},
+                {0, 9, 8, 0, 0, 0, 0, 6, 0},
+                {8, 0, 0, 0, 6, 0, 0, 0, 3},
+                {4, 0, 0, 8, 0, 3, 0, 0, 1},
+                {7, 0, 0, 0, 2, 0, 0, 0, 6},
+                {0, 6, 0, 0, 0, 0, 2, 8, 0},
+                {0, 0, 0, 4, 1, 9, 0, 0, 5},
+                {0, 0, 0, 0, 8, 0, 0, 7, 9}
+        };
+
+        System.out.println("Before solving:");
+        printBoard(board);
+
+        if (solve(board)) {
+            System.out.println("After solving:");
+            printBoard(board);
+        } else {
+            System.out.println("No solution exists!");
+        }
+    }
+}
+
+
