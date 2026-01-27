eee309a923261cec598fbb603c86ebabebaf6f82
K-Anjan25 <132475784+K-Anjan25@users.noreply.github.com>
2026-01-27 19:01:10 +0530

    Add ConsumerDemo class to demonstrate Consumer usage

---
+++ TNS.java/src/com/tnsif/java/day14/ConsumerDemo.java
@@ -0,0 +1,15 @@
+package com.tnsif.java.day14;
+
+import java.util.function.Consumer;
+
+public class ConsumerDemo {
+
+	public static void main(String[] args) {
+		
+		Consumer<String> len=str->System.out.println(str.length());
+		String[] arr= {"Gopi","Ram","Krishna"};
+		for(String k:arr)
+			len.accept(k);
+	}
+
+}
