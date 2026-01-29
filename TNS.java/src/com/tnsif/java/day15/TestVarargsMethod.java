5c426b549b87698134de14666d3b0a868b4915dd
K-Anjan25 <132475784+K-Anjan25@users.noreply.github.com>
2026-01-29 18:40:48 +0530

    Add VarargsMethod and TestVarargsMethod classes

---
+++ TNS.java/src/com/tnsif/java/day15/TestVarargsMethod.java
@@ -0,0 +1,21 @@
+package com.tnsif.java.day15;
+
+
+public class VarargsMethod {
+    @SafeVarargs
+    public final void printValues(String... values) {
+        for (String value : values) {
+            System.out.println(value);
+        }
+    }
+}
+
+
+
+public class TestVarargsMethod {
+    public static void main(String[] args) {
+        VarargsMethod varargsMethod = new VarargsMethod();
+        varargsMethod.printValues("Hello", "World", "!");
+    }
+}
+
