c3976d80eeb269cdcba9e5fbc4ef5797184e1f1c
K-Anjan25 <132475784+K-Anjan25@users.noreply.github.com>
2026-01-30 09:06:55 +0530

    Add StreamExample class for filtering strings

---
+++ TNS.java/src/com/tnsif/java/day15/StreamExample.java
@@ -0,0 +1,23 @@
+package com.tnsif.java.day15;
+
+import java.util.*;
+import java.util.stream.*;
+
+public class StreamExample {
+public static void main(String[] args) {
+List<String> list = Arrays.asList("Apple"
+,
+"Banana"
+,
+"Cherry"
+,
+
+"Date");
+
+// Stream operation to filter and print items with length
+greater than 5
+list.stream()
+.filter(item -> item.length() > 5)
+.forEach(System.out::println);
+}
+}
