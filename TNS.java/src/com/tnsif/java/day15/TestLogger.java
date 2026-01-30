a15ed8da9d09b9a80fb8e422bfbbade052a6358b
K-Anjan25 <132475784+K-Anjan25@users.noreply.github.com>
2026-01-30 09:24:49 +0530

    Rename Logger class to TestLogger and adjust usage

--- TNS.java/src/com/tnsif/java/day15/TestLogger.java
+++ TNS.java/src/com/tnsif/java/day15/TestLogger.java
@@ -1,16 +1,13 @@
 package com.tnsif.java.day15;
 
 
-class Logger {
+public class TestLogger {
+
     @SuppressWarnings("unused")
     public void log(String message) {
         int unusedVariable = 10;
         System.out.println("Log: " + message);
     }
-}
-
-
-public class TestLogger {
     public static void main(String[] args) {
         Logger logger = new Logger();
         logger.log("Hello, World!");
