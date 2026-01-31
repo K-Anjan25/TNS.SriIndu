1c25dbdc6a204533594e040b5cc4041742eccf36
K-Anjan25 <132475784+K-Anjan25@users.noreply.github.com>
2026-01-31 09:03:37 +0530

    Update TestLogger.java

--- TNS.java/src/com/tnsif/java/day15/TestLogger.java
+++ TNS.java/src/com/tnsif/java/day15/TestLogger.java
@@ -3,10 +3,12 @@ package com.tnsif.java.day15;
 
 public class TestLogger {
 
+    public class Logger{
     @SuppressWarnings("unused")
     public void log(String message) {
         int unusedVariable = 10;
         System.out.println("Log: " + message);
+    }
     }
     public static void main(String[] args) {
         Logger logger = new Logger();
