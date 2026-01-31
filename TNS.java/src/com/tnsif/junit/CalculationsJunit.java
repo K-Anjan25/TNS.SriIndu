0a1a2af55159162f432e8b929e96e61538e95a51
Anjan Kumar <kyamaanjan2@example.com>
2026-01-31 18:27:32 +0530

    added Junit

---
+++ TNS.java/src/com/tnsif/junit/CalculationsJunit.java
@@ -0,0 +1,16 @@
+package com.tnsif.junit;
+
+
+class CalculationsJunit {
+
+	public int add(int a,int b) {
+			return a+b;
+		}
+		public int divide(int a,int b) {
+			return a/b;
+		}
+		public String getName() {
+			return "Junit";
+		}
+	
+}
